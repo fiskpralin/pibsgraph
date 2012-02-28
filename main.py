42f24b8a8bb4f519941c4743aef2d204fb1a2251
Linus Jundén <linus.junden@gmail.com>
2012-02-28 09:34:16 +0100

    Linus Test1

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ if __name__=='__main__':
 		import cProfile
 		from sim.planting.plantMTerrain import PlantMTerrain
 		seed=int(random.uniform(0,100000))
-		seed=55875
+		seed=55874
 		random.seed(seed)
 		print "seed2:", seed
 		G=tools.globalVar()
