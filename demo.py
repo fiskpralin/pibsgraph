599d72954519bc0639daf633ca5a4daea3e9f18f
fishy <mattias.lindh@math.umu.se>
2012-02-28 11:35:27 +0100

    Mattias erroneous but healthy demo.py added

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-from sim.tools import globalVar,SimExtend # a global veriable and simextend cladd that does a lot of overhead stuff.
+from sim.tools import globalVar, SimExtend #a global veriable and simextend class that does a lot of overhead stuff.
 import terrain
 from terrain.terrain import Terrain, Tree
 from plot import * #does all the plotting
@@ -6,12 +6,12 @@ import machines
 from machines.basics import Machine #this class is common for all machines
 from functions import *
 import random
-
+from collision import collide
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 import numpy as np
-
+from math import *
 #this is a minimal simulation. 
 
 class DumbMachine(Machine):
@@ -21,25 +21,43 @@ class DumbMachine(Machine):
 	def __init__(self, sim, G, color):
 		Machine.__init__(self, sim=sim, name='dumbass', G=G)
 		self.velocities['machine']=3 #m/s, default name, has to be there
-		self.setPos([1,1])
+		self.setPos([15,0])#Sets the initial position to random in the x direction and 0 in y
 		self.IQ=10 #as I said... no AI here
 		self.color=color
+		self.trees=[]
 	def run(self):
 		"""
 		the process execution method
 		"""
 		self.positions=[self.pos] #save, will later be plotted
-		t=self.G.terrain #shorter, clearer code
-		while self.sim.now()<1000: #while time of the simulation is below 1000
-			newX=random.uniform(t.xlim[0], t.xlim[1])
-			newY=random.uniform(*tuple(t.ylim)) #another way to do the same thing.
-			time=self.setPos([newX, newY]) #we don't check for collission with other machines and the tree.. a flaw..
-			if skörda:
-				tree.isSpherical=False
-				tree.pos=[10,10]
-				tree.nodes=[()]
+		t=self.G.terrain #used to make code shorter and clearer
+		while self.pos[1]<t.ylim[1]: #while machine is inside the domain
+			newX=self.pos[0]
+			newY=self.pos[1]+random.uniform(1,5)
+			temp_treelist=t.GetTrees(self.pos,10)
+			for a in temp_treelist:
+				if a.harvested==False:
+					a.harvested=True
+					a.isSpherical=False
+					a.nodes=[[a.pos[0]-6,a.pos[1]-0.1],[a.pos[0]-6,a.pos[1]+0.1],[a.pos[0],a.pos[1]+0.1],[a.pos[0],a.pos[1]-0.1]]
+					self.trees.append(a)
+					yield hold,self, 30#the time it takes to chop the tree (half a minute!)			
+			time=self.setPos([newX, newY])#the time it takes to move to the new place
 			self.positions.append(self.pos)
 			yield hold, self, time #waits
+		"""Now all the trees should be harvested and so I tell the machine to go fetch them!"""
+		for a in self.trees:
+			newX=a.pos[0]
+			newY=a.pos[1]
+			time=self.setPos([newX,newY])
+			self.positions.append([newX,newY])
+			print "Moving out again to the harvested trees to pick them up"
+			yield hold, self, time
+		newPos=self.positions[0]
+		self.positions.append(newPos)
+		time=self.setPos(newPos)
+		print "Moving home with all the trees which I have picked up in a stupid random order"
+		yield hold, self, time
 		self.sim.stopSimulation()
 	def draw(self,ax):
 		"""
@@ -51,35 +69,58 @@ class DumbMachine(Machine):
 			x.append(pos[0])
 			y.append(pos[1])
 		ax.plot(x,y, self.color)
-		corners=[]
-		dxdy=1 #create a square..
-		corners.append([self.pos[0]+dxdy, self.pos[1]+dxdy])
-		corners.append([self.pos[0]-dxdy, self.pos[1]+dxdy])
-		corners.append([self.pos[0]-dxdy, self.pos[1]-dxdy])
-		corners.append([self.pos[0]+dxdy, self.pos[1]-dxdy])		
-		p=Polygon(np.array(corners), closed=True, facecolor=self.color)
+		self.corners=[]
+		dx=1 #width of the machine
+		dy=2 #length of the machine
+		self.corners.append([self.pos[0]+dx, self.pos[1]+dy])
+		self.corners.append([self.pos[0]-dx, self.pos[1]+dy])
+		self.corners.append([self.pos[0]-dx, self.pos[1]-dy])
+		self.corners.append([self.pos[0]+dx, self.pos[1]-dy])		
+		p=Polygon(np.array(self.corners), closed=True, facecolor=self.color)#here was the np.array(self.corners) command before
 		ax.add_patch(p)
+
+	def makedirectional(self):
+		"""
+		makes new positions of the nodes of the machine as to draw it in the correct direction
+		honestly i don't think anythin is correct in this 'attempt'. stupid-
+		"""
+		widthx=self.pos[0]-self.positions[-1][0]
+		if widthx==0: widthx=0.01
+		widthy=self.pos[1]-self.positions[-1][1]
+		r=[]
+		for i in [0,1,2,3]:
+			r.append(sqrt(self.corners[i][0]*self.corners[i][0]+self.corners[i][1]*self.corners[i][1]))
+		direction=atan(widthy/widthx)
+		b=[]
+		for i in [0,1,2,3]:
+			b.append(self.corners[i]+[r[i]*cos(direction),r[i]*sin(direction)])
+		return b
+		
+
+
 class TheSimulation(SimExtend):
 	"""
 	specifies the simulation
 	"""
 	def __init__(self):
-		G=globalVar() #create a global variable.
-		G.plotDelay=5 #we will plot each 5th second. If you want this, do it before SimExtend.__init__
-		SimExtend.__init__(self,G=G, vis=True) #does a lot of overhead stuff. can be found in sim.tools.py
-		self.G.areaPoly=[(0,0), (40,0), (40,40), (0,40)] #specifies our world borders.
+		G=globalVar() #create a global variable. 
+		SimExtend.__init__(self,G=G)
+		G.xlim=[0, 40]#the borders.
+		G.ylim=[0,40] 
 		self.G.terrain=Terrain(G=self.G)
-		t=Tree(pos=[3,4], radius=0.5, specie='pine', terrain=self.G.terrain)
+		self.tree=Tree(pos=[20,25], radius=0.5, specie='pine', terrain=self.G.terrain)
 		m1=DumbMachine(sim=self, G=G, color='b') #color is definitely the most important thing here.
-		m2=DumbMachine(sim=self, G=G, color='r') #sim=self.. self is here refering to TheSimulation
-		for m in [m1,m2]:
+		m1.velocities['machine']=0.5
+		G.plotDelay=20
+		self.p=PlotWorld(name='plotter',sim=self, G=G) #create a plotter instance...
+		
+		for m in [m1]:
 			self.activate(m, m.run()) #activate machines
-		self.activate(self.p,self.p.run())
+		self.activate(self.p, self.p.run())#activate some plotting thing i dont fully understand
 		self.simulate(until=1e10)
-		if self.p: #means that we have a plotting instance. All done in SimExtend.
-			self.p.update() #abort p.
-			print "simulation done"
-			plt.show()
+		self.p.update() #abort p.
+		print "simulation done"
+		plt.show()
 
 if __name__=='__main__':
 	s=TheSimulation()
