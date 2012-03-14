008c04d27993822225f37eae78862a61b234d0cc
fishy <mattias.lindh@math.umu.se>
2012-03-14 10:41:35 +0100

    Implemented a pile-class in which the dumped trees are collected. some coordinates and properties need be changed, with the file ;)

---
+++ terrain/pile.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+from math import *
+from collision import *
+from functions import *
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+from obstacle import *
+
+
+class Pile(Obstacle):
+	def __init__(self, pos, biomass=None, diameter=None, radius=None, terrain=None , length=None, weight=None, logWeight=None, vol=None):
+		Obstacle.__init__(self, pos, isSpherical=False, radius=radius, terrain=terrain, color='brown')# '#5C3317'after debug
+		self.pos=pos
+		self.radius=radius #This Radius thingy is for collision detection, don't bother about it.
+#		if not vol:	vol=(dbh*0.5)**2*pi*height*0.333 #cone
+		self.vol=vol
+		self.trees=[]
+		self.biomass=biomass
+		self.length=length
+		self.diameter=diameter
+		self.name = "Pile"
+		if terrain: terrain.piles.append(self)#remove this to handle it manually in dumpTrees?
+		self.nodes=None
+
+	def updatePile(self, direction=None):
+		self.biomass = sum([t.weight for t in self.trees])
+		self.length = max([t.h for t in self.trees])
+		self.maxDiam = max([t.dbh for t in self.trees])
+		self.diameter = 0.1748 + 0.0345 * self.maxDiam*100 # This model is given by Ola and Dan 2012.03.10
+		self.radius = sqrt(self.length**2+(self.diameter/2)**2)
+
+		if direction is None: raise('EXCEPTION: updatePile needs the direction of the Road')
+
+		#here the nodes of the pile are set
+		c1=getCartesian([-self.diameter/2,self.length/2], origin=self.pos, direction=direction, fromLocalCart=True)
+		c2=getCartesian([-self.diameter/2, 0], origin=self.pos, direction=direction, fromLocalCart=True)
+		c3=getCartesian([self.diameter/2, 0], origin=self.pos, direction=direction, fromLocalCart=True)
+		c4=getCartesian([self.diameter/2,self.length/2], origin=self.pos, direction=direction, fromLocalCart=True)
+		self.nodes=[c1,c2,c3,c4]
+		
+	def getNodes(self, pos=None):
+		if not self.nodes:
+			return False
+		else:
+			return self.nodes
+		
+	def draw(self, ax):
+		if self.nodes: #plot as polygon.
+			ax.add_patch(mpl.patches.Polygon(np.array(self.nodes), closed=True, facecolor=self.color, alpha=85))
+		else:
+			raise('EXCEPTION: The pile must have nodes.')
